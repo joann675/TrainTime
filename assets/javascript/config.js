01b6b3be5ec64b111b071c14edb099fa7bf92744
joann675 <joann675@yahoo.com>
2019-01-17 18:09:06 -0500

    added config file

---
+++ assets/javascript/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBQf70pI5VfxEayXWiw2ykFceB_KFOx2Mk",
+  authDomain: "joann-sfirebase.firebaseapp.com",
+  databaseURL: "https://joann-sfirebase.firebaseio.com",
+  projectId: "joann-sfirebase",
+  storageBucket: "joann-sfirebase.appspot.com",
+  messagingSenderId: "435142571720"
+};
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,3 @@
-
 var myConfig = {
   apiKey: "",
   authDomain: "",
